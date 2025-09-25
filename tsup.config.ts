40e46d62eb0f7af6acccbc171f5394ddbcb84dcf
Stefan Knoblich <stkn@bitplumber.de>
2025-09-25 17:24:08 +0200

    Remove CJS support
    Signed-off-by: Stefan Knoblich <stkn@bitplumber.de>

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/aegis128l.mts", "src/aegis256.mts"],
-  format: ["cjs", "esm"],
+  format: ["esm"],
   dts: true,
   outDir: "dist",
   clean: true,
